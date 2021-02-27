43672fe0b19e61b21da3e41fef7d680e576bd8f5
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-02-27 17:39:02 -0600

    Data Respone
    Getting Data Object back from API

--- github_user_cards/src/App.js
+++ github_user_cards/src/App.js
@@ -1,25 +1,50 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      github: []
+    }
+  }
+
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/cleveroscar')
+      .then((res) => this.setState({github: res.data}))
+      .catch((err) => console.log(err))
+
+
+
+    // axios.get('/user?ID=12345')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   })
+    //   .then(function () {
+    //     // always executed
+    //   });
+  }
+
+
+  
+  render() {
+
+      console.log(this.state)
+      return (
+      <div className="App">
+        Github User Cars
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
